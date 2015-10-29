6c05e2aa81b250ca6915849a7dd236a0271e9628
Kévin Ta <kevin.ta@edu.esiee.fr>
2015-10-29 23:52:58 +0100

    Create JobBundle + UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Zephyr\UserBundle\ZephyrUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Zephyr\JobBundle\ZephyrJobBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
