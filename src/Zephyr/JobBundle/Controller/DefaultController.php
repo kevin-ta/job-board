e8edc247ca59c740e578a9c09dd2efe33b1abdf1
Kévin Ta <kevin.ta@edu.esiee.fr>
2015-11-02 20:28:44 +0100

    Update controller

--- src/Zephyr/JobBundle/Controller/DefaultController.php
+++ src/Zephyr/JobBundle/Controller/DefaultController.php
@@ -8,6 +8,22 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ZephyrJobBundle:Default:index.html.twig');
+    	$em = $this->getDoctrine()->getManager();
+    	$jobs = $em->getRepository('ZephyrJobBundle:Job')->findAll();
+    	$total = count($jobs);
+    	$valid = 0;
+    	for ($i = 0; $i < $total; $i++)
+    	{
+    		if ($status = $jobs[$i]->getValid() != null)
+    		{
+    			$valid = $valid + 1;
+    		}
+    	}
+
+        return $this->render('ZephyrJobBundle:Default:index.html.twig', array(
+            'jobs' => $jobs,
+            'total' => $total,
+            'valid' => $valid,
+        ));
     }
 }
