8f0d5cf8d1d78dcf96d5ce9739e2d41e47033d1e
Kévin Ta <kevin.ta@edu.esiee.fr>
2015-11-03 09:53:16 +0100

    Add layout for registration

---
+++ src/Zephyr/UserBundle/Form/RegistrationType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Zephyr\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstname')
+                ->add('lastname');
+    }
+
+    public function getParent()
+    {
+        return 'fos_user_registration';
+    }
+
+    public function getName()
+    {
+        return 'app_user_registration';
+    }
+}
